e67e725ca17c5885dd4f37a58b91a77843834680
Yu-Shen Ng <yng@yn-mbp-jrm6f.tail5955.ts.net>
2025-11-25 13:55:51 -0800

    Change port from 5000 to 5001

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ if __name__ == '__main__':
     # Initialize database on startup
     init_db()
     # Run the Flask app
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
